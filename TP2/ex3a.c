1a4dbba70de16814d600b97e5082f59ad7624c51
Bravej <bravej@localhost.localdomain>
2017-11-30 21:39:58 +0100

    update

--- TP2/ex3a.c
+++ TP2/ex3a.c
@@ -8,52 +8,78 @@
 #include <time.h>
 #include <semaphore.h> 
 
-#define NO_PLACES 3
-#define NO_CARS   5
+#define NO_PLACES 2
+#define NO_CARS   3
 
 
-sem_t places;//[NO_PLACES]; 
+sem_t places;
 
-void enterParking(int carId){
+/*
+ * @param : entier qui est l identifiant de la voiture
+ * @brief : decremente le semaphore, ce qui represente au fait 
+ *          qu'une voiture a prit une place disponible.
+ *					j'ai changer le nom de la fonction, puique sémantiquement
+ *          elle correspond plutot à une prise de place qu'au fait 
+ *          qu'une voiture rentre dans le parking
+ */
+
+void prendrePlace(int carId){
 	printf("Voiture %d arrive dans le parking \n", carId);
 	sem_wait(&places);
 	printf("Voiture %d a trouve une place \n", carId);
 }
 
-void leaveParking(int carId){
+/*
+ * @param : entier qui est l identifiant de la voiture
+ * @brief : increment le semaphore, ce qui represente au fait 
+ *          qu'une voiture a libere une place.
+ *					j'ai changer le nom de la fonction, puique sémantiquement
+ *          elle correspond plutot à la liberation d'une place et non à une 
+ * 				  voiture qui sort du parking 
+ */
+void quitterPlace(int carId){
 	sem_post(&places);
-	printf("Voiture %d est sortie \n", carId);
+	printf("Voiture %d a quitter la place \n", carId);
 }
 
+/*
+ * @param : entier qui est l identifiant de la voiture
+ * @brief : Cycle de vie d'une voiture. J'ai placé des sleep avec valeur 
+ *          aleatoire (les voitures n'arrivent pas ensemble, et reste un
+ *          certain temps dans le parking). En soit dans cette fonction : 
+ *          la voiture arrive, entre dans le parking prend une place 
+ *          si il y en a une disponible, sinon elle attend. Puis elle 
+ *          attend un temps aléatoire et quitte le parking
+ */
 void* carLife(void * carId){
 	int enterTime = rand() % 10;
 	int leaveTime = rand() % 10;
 	printf("Voiture %d attend %d s avant d'entrer\n", (int) carId, enterTime);
 	sleep(enterTime);
-	enterParking((int)carId);
+	prendrePlace((int)carId);
 	printf("Voiture %d attend %d s avant de sortir\n", (int) carId, leaveTime);
 	sleep(leaveTime);
-	leaveParking((int)carId);
+	quitterPlace((int)carId);
 }
 
 
 int main(){
 	int i;
  	srand(time(NULL)); // initialisation de rand	
-	pthread_t threadCars[NO_CARS];
+	pthread_t threadCars[NO_CARS]; // autant de thread que de voiture
 	
-	sem_init(&places, 0, NO_PLACES);  		
+	sem_init(&places, 0, NO_PLACES); // notre semaphoreest initialisé au nombre de place disponible
 
 	for (i = 0; i < NO_CARS; ++i){
 	 	if(pthread_create(&threadCars[i], NULL, carLife, (void*)i) == -1) {
 			perror("pthread_create");
 			return -1;
-		}
+		} 
 
-	}
+	} // création de tout les threads
 	for (i = 0; i < NO_CARS; ++i){		
 		pthread_join(threadCars[i], NULL);
-	}
+	} // on attend qu'ils finnissent tous
 }
 
 
--- TP2/ex3b.c
+++ TP2/ex3b.c
@@ -6,61 +6,127 @@
 #include <time.h>
 #include <semaphore.h> 
 
-#define NO_PLACES  3
-#define NO_CARS    5
+#define NO_PLACES  1
+#define NO_CARS    3
 #define NO_ENTRIES 2
 #define NO_EXITS   2 
 
 sem_t places; 
 sem_t entrees;
 sem_t sorties;
-pthread_cond_t condNoPlace = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t mutex ;//= PTHREAD_MUTEX_INITIALIZER;
 
+int nbCarEntree = 0; // correspond aux nombres de voiture dans le parking
+	// Ce compte me permet de bloquer les voitures dans l'entree si il n'y 
+  // a plus de place disponnible
+
+
+/*
+ * @param : entier qui est l identifiant de la voiture
+ * @brief : cette fonction correspond a l'entree d'une voiture.
+ *          La voiture entre dans l'entree, decremente le semaphore
+ *          puis des quelle sort incremente le semaphore. Cette fonction
+ *          permet également d'empecher les voitures de rentrer si il 
+ *          n'y a plus de place disponnible. 
+ */ 
 void entrer(int carId){
-	printf("Voiture %d arrive dans lentree \n", carId);
-	pthread_mutex_unlock(&mutex);
+	++nbCarEntree; // incrementre le nombre de voiture presente		
+	printf("Voiture %d fait la queue dans lentree \n", carId);
+	
+	sem_wait(&entrees); // on attend dans l'entree
+
+	if (nbCarEntree > NO_PLACES){ // si il y a plus de voiture que de place
+		int entree;
+		do { // tant que le semaphore est different de 0
+			sem_getvalue(&entrees, &entree);	// recupere la valeur du semaphore
+			
+			sem_wait(&entrees);		
+			
+		}	while (entree-1); // on fait attendre toutes les voitures dans l'entree
+		
+	}	
+	
+
+	sleep(1);	
+
 	printf("Voiture %d sortie entree \n", carId);
-	pthread_mutex_lock(&mutex);
+	sem_post(&entrees); // la voiture est sortie
 }
 
+
+/*
+ * @param : entier qui est l identifiant de la voiture
+ * @brief : decremente le semaphore, ce qui represente au fait 
+ *          qu'une voiture a prit une place disponible.
+ *					j'ai changer le nom de la fonction, puique sémantiquement
+ *          elle correspond plutot à une prise de place qu'au fait 
+ *          qu'une voiture rentre dans le parking. Dans le contexte de 
+ *          cette exercice, le semephore sur le nombre de place disponible 
+ * 				  ne sert plus a grand chose, puisque  les voitures sont regulés
+ *          dans l'entree 
+ */
+
+void prendrePlace(int carId){
+	sem_wait(&places); // on attend une place
+	printf("Voiture %d a trouve une place \n", carId);
+}
+
+/*
+ * @param : entier qui est l identifiant de la voiture
+ * @brief : increment le semaphore, ce qui represente au fait 
+ *          qu'une voiture a libere une place.
+ *					j'ai changer le nom de la fonction, puique sémantiquement
+ *          elle correspond plutot à la liberation d'une place et non à une 
+ * 				  voiture qui sort du parking 
+ */
+void quitterPlace(int carId){
+	sem_post(&places); // libere une place
+	printf("Voiture %d libere la place \n", carId);
+	if (nbCarEntree >= NO_PLACES){
+		int i;		
+		for (i = 0; i < NO_ENTRIES; ++i){
+			sem_post(&entrees);
+			--nbCarEntree;
+		}		
+		sleep(1);
+	} // si le toutes les places étaient deja prisent, 
+	// les voitures etaient bloquees dans l'entree. Ici on libere une ressource
+}
+
+/*
+ * @param : entier qui est l identifiant de la voiture
+ * @brief : cette fonction correspond a la sortie d'une voiture.
+ *          la voiture entre dans la sortie, decremente le semaphore
+ *          puis des quelle sort incremente le semaphore 
+ */
 void sortie(int carId){
 	printf("Voiture %d arrive dans la sortie \n", carId);
 	sem_wait(&sorties);
 	sleep(1);
 	printf("Voiture %d sortie de sortie \n", carId);
 	sem_post(&sorties);
+	
 }
 
-void leaveParking(int carId){
-	sem_post(&places);
-	pthread_cond_signal(&condNoPlace);
-	printf("Voiture %d est sortie \n", carId);
-}
-
-void enterParking(int carId){
-	int isWaiting;
-	printf("Voiture %d arrive dans le parking \n", carId);	
-	sem_getvalue(&places, &isWaiting);	
-	if (!isWaiting){
-		printf("Voiture bloque à l entree\n");	
-		pthread_cond_wait(&condNoPlace, &mutex);	
-	}	// si plus de place on bouche les entrees
-	sem_wait(&places);
-	printf("Voiture %d a trouve une place \n", carId);
-}
-
-
+/*
+ * @param : entier qui est l identifiant de la voiture
+ * @brief : Cycle de vie d'une voiture. J'ai placé des sleep avec valeur 
+ *          aleatoire (les voitures n'arrivent pas ensemble, et reste un
+ *          certain temps dans le parking). En soit dans cette fonction : 
+ *          la voiture arrive dans l'entree si il y en a une des disponible,
+ *					entre dans le parking prend une place 
+ *          si il y en a une de disponible, sinon elle attend. Puis elle 
+ *          attend un temps aléatoire libere la place et va dans une sortie 
+ *          si il y en a une de disponible
+ * 
+ */
 void* carLife(void * carId){
-	int enterTime = rand() % 10;
-	int leaveTime = rand() % 10;
-//	printf("Voiture %d attend %d s avant d'entrer\n", (int) carId, enterTime);
-	sleep(enterTime);
+	int enterTime = rand() % 10; // temps pour entrer random
+	int leaveTime = rand() % 10; // temps pour sortir random
+//	sleep(enterTime);
 	entrer((int)carId);
-	enterParking((int)carId);
-//	printf("Voiture %d attend %d s avant de sortir\n", (int) carId, leaveTime);
+	prendrePlace((int)carId);
 	sleep(leaveTime);
-	leaveParking((int)carId);
+	quitterPlace((int)carId);
 	sortie((int)carId);
 }
 
@@ -68,21 +134,21 @@ void* carLife(void * carId){
 int main(){
 	int i;
  	srand(time(NULL)); // initialisation de rand	
-	pthread_t threadCars[NO_CARS];
+	pthread_t threadCars[NO_CARS]; // autant de threads que de voitures
 	
+	// initialisation de tous les semaphores avec la constante correspondante
 	sem_init(&places,  0, NO_PLACES);  		
 	sem_init(&entrees, 0, NO_ENTRIES);  		
 	sem_init(&sorties, 0, NO_EXITS);  		
-	pthread_mutex_init(&mutex, NULL);
 	for (i = 0; i < NO_CARS; ++i){
 	 	if(pthread_create(&threadCars[i], NULL, carLife, (void*)i) == -1) {
 			perror("pthread_create");
 			return -1;
 		}
 
-	}
+	}// création de tout les threads
 	for (i = 0; i < NO_CARS; ++i){		
 		pthread_join(threadCars[i], NULL);
-	}
+	} // on attend qu'ils finnissent tous
 }
 
